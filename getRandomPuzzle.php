bcdb96a971c95f9686ec139a4ddd463c66814f35
Beckett Randlett <beckettrandlettapps@gmail.com>
2023-05-01 20:47:35 +0000

    add color choosing frontend, fix backend

--- getRandomPuzzle.php
+++ getRandomPuzzle.php
@@ -3,7 +3,7 @@ $tag = $_GET['tag'];
 $color = $_GET['color'];
 include 'db_connection.php';
 $conn = OpenCon();
-$query = "SELECT * FROM Puzzle WHERE tags = '" . ( ($tag == 'none') ? "' OR '1'='1'" : $tag . "'") .  " AND toMove='" . ( ($color == 'either') ? "' OR '1'='1'" : $color . "' AND Solutions IS NOT NULL ORDER BY RAND() LIMIT 1"); 
+$query = "SELECT * FROM Puzzle WHERE (tags = '" . ( ($tag == 'none') ? "' OR '1'='1'" : $tag . "'") .  ") AND (toMove='" . ( ($color == 'either') ? "' OR '1'='1'" : $color . "'") . ") AND Solutions IS NOT NULL ORDER BY RAND() LIMIT 1"; 
 if ($result = mysqli_query($conn, $query)) {
 	while ($rowData = mysqli_fetch_array($result)) {
 			echo $rowData["PuzzleID"].'<br>';
