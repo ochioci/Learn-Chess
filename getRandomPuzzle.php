06ecfecd38f82e51b1e9be2e98e788019c1dce10
ochioci <63116595+ochioci@users.noreply.github.com>
2023-05-01 16:32:15 -0400

    add color choosing

--- getRandomPuzzle.php
+++ getRandomPuzzle.php
@@ -1,23 +1,16 @@
 <?php
 $tag = $_GET['tag'];
+$color = $_GET['color'];
 include 'db_connection.php';
 $conn = OpenCon();
-if ($tag != 'none') {
-	if ($result = mysqli_query($conn, "SELECT * FROM Puzzle WHERE tags = '" . $tag .  "' AND Solutions IS NOT NULL ORDER BY RAND() LIMIT 1")) {
-  	while ($rowData = mysqli_fetch_array($result)) {
-      		echo $rowData["PuzzleID"].'<br>';
-      		echo $rowData["Position"].'<br>';
-     		echo $rowData["Solutions"].'<br>';
-  		}
-	}
-} else {
-	if ($result = mysqli_query($conn, "SELECT * FROM Puzzle WHERE Solutions IS NOT NULL ORDER BY RAND() LIMIT 1")) {
-		while ($rowData = mysqli_fetch_array($result)) {
+$query = "SELECT * FROM Puzzle WHERE tags = '" . ( ($tag == 'none') ? "' OR '1'='1'" : $tag . "'") .  " AND toMove='" . ( ($color == 'either') ? "' OR '1'='1'" : $color . "' AND Solutions IS NOT NULL ORDER BY RAND() LIMIT 1"); 
+if ($result = mysqli_query($conn, $query)) {
+	while ($rowData = mysqli_fetch_array($result)) {
 			echo $rowData["PuzzleID"].'<br>';
 			echo $rowData["Position"].'<br>';
-			echo $rowData["Solutions"].'<br>';
+		   echo $rowData["Solutions"].'<br>';
 		}
-	}
-}
+  }
+
 CloseCon($conn);
 ?>
