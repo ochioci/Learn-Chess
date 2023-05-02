3d64a7e6ee95773a59af6f8a7b6ec6be865236d5
beckettr <beckettr@Linux.myguest.virtualbox.org>
2023-05-02 19:53:25 -0400

    no sql injection!

--- getRandomPuzzle.php
+++ getRandomPuzzle.php
@@ -3,9 +3,11 @@ ini_set('display_errors',1);
 error_reporting(E_ALL);
 $tag = $_GET['tag'];
 $color = $_GET['color'];
+$tag = preg_replace("/[^a-z]+/", "", $tag);
+$color = preg_replace("/[^a-z]+/", "", $color);
 include 'db_connection.php';
 $conn = OpenCon();
-$query = "SELECT * FROM Puzzle WHERE (tags = '" . ( ($tag == 'none') ? "' OR '1'='1'" : $tag . "'") .  ") AND (toMove='" . ( ($color == 'either') ? "' OR '1'='1'" : $color . "'") . ") AND Solutions IS NOT NULL ORDER BY RAND() LIMIT 1"; 
+$query = "SELECT * FROM Puzzle WHERE (tags = '" . ( ($tag == 'none') ? "' OR '1'='1'" : $tag . "'") .  ") AND (toMove='" . ( ($color == 'either') ? "' OR '1'='1'" : $color . "'") . ") AND Solutions IS NOT NULL ORDER BY RAND() LIMIT 1";
 if ($result = mysqli_query($conn, $query)) {
 	while ($rowData = mysqli_fetch_array($result)) {
 			echo $rowData["PuzzleID"].'<br>';
