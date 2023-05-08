126d8234be6e075aec4586eec1fe690d0781f99b
beckettr <beckettr@Linux.myguest.virtualbox.org>
2023-05-08 14:52:46 -0400

    add max solutions qsp

--- getRandomPuzzle.php
+++ getRandomPuzzle.php
@@ -5,9 +5,11 @@ $tag = $_GET['tag'];
 $color = $_GET['color'];
 $tag = preg_replace("/[^a-z]+/", "", $tag);
 $color = preg_replace("/[^a-z]+/", "", $color);
+$maxSolutions = $_GET["maxSolutions"];
 include 'db_connection.php';
 $conn = OpenCon();
-$query = "SELECT * FROM Puzzle WHERE (tags = '" . ( ($tag == 'none') ? "' OR '1'='1'" : $tag . "'") .  ") AND (toMove='" . ( ($color == 'either') ? "' OR '1'='1'" : $color . "'") . ") AND Solutions IS NOT NULL ORDER BY RAND() LIMIT 1";
+$query = "SELECT * FROM Puzzle WHERE (tags = '" . ( ($tag == 'none') ? "' OR '1'='1'" : $tag . "'") .  ") AND (toMove='" . ( ($color == 'either') ? "' OR '1'='1'" : $color . "'") . ") AND Solutions IS NOT NULL AND CHAR_LENGTH(Solutions) < " . strval((intval($maxSolutions) * 5) + 1) . " ORDER BY RAND() LIMIT 1;";
+echo $query;
 if ($result = mysqli_query($conn, $query)) {
 	while ($rowData = mysqli_fetch_array($result)) {
 			echo $rowData["PuzzleID"].'<br>';
