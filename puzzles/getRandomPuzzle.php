f39f498bb8cc60e3e5204812722427693a296968
ochioci <63116595+ochioci@users.noreply.github.com>
2023-05-19 13:37:12 -0400

    use maxSolutions column instead of char_length

--- puzzles/getRandomPuzzle.php
+++ puzzles/getRandomPuzzle.php
@@ -4,12 +4,13 @@ ini_set('display_errors',1);
 error_reporting(E_ALL);
 $tag = $_GET['tag'];
 $color = $_GET['color'];
+$maxSolutions = $_GET["maxSolutions"];
 $tag = preg_replace("/[^a-z]+/", "", $tag);
 $color = preg_replace("/[^a-z]+/", "", $color);
-$maxSolutions = $_GET["maxSolutions"];
+$maxSolutions = preg_replace("/[^0-9]+/", "", $maxSolutions);
 include '../util/db_connection.php';
 $conn = OpenCon();
-$query = "SELECT * FROM Puzzle WHERE (tags = '" . ( ($tag == 'none') ? "' OR '1'='1'" : $tag . "'") .  ") AND (toMove='" . ( ($color == 'either') ? "' OR '1'='1'" : $color . "'") . ") AND Solutions IS NOT NULL AND CHAR_LENGTH(Solutions) < " . strval((intval($maxSolutions) * 5) + 1) . " ORDER BY RAND() LIMIT 1;";
+$query = "SELECT * FROM Puzzle WHERE (tags = '" . ( ($tag == 'none') ? "' OR '1'='1'" : $tag . "'") .  ") AND (toMove='" . ( ($color == 'either') ? "' OR '1'='1'" : $color . "'") . ") AND Solutions IS NOT NULL AND numSolutions <= " . $maxSolutions . " ORDER BY RAND() LIMIT 1;";
 // echo $query;
 if ($result = mysqli_query($conn, $query)) {
 	if (isset($_SESSION["user"])) {
